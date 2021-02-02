b1b51d37c0d38de8c4878e10718f44d802f829b3
= <zainr7989@gmail.com>
2021-02-02 14:39:32 -0500

    Refactor whitespaces.

--- lab/refactoring-pep8/whitespace_naming_convention.py
+++ lab/refactoring-pep8/whitespace_naming_convention.py
@@ -4,19 +4,19 @@
 import random
 
 
-lower_limit=0
-upper_limit= 100
+lower_limit = 0
+upper_limit = 100
 random_number = random.randint(lower_limit, upper_limit)
 print(\
     "The computer has selected a number between 0 and 100. Use your supernatural superpowers to guess what the number is."
 )
 while True:
-    user_guess = int (input("Enter a number between 0 and 100 (including): "))
+    user_guess = int(input("Enter a number between 0 and 100 (including): "))
     if user_guess > random_number:
-        print ("Guess a smaller number.")
+        print("Guess a smaller number.")
     elif user_guess < random_number:
-        print ("Guess a larger number.")
-    else: #UserGuess == randomNumber:
-        print ("You Won!")
+        print("Guess a larger number.")
+    else:  # user_guess == randomNumber:
+        print("You Won!")
         break
     
--- lab/refactoring-pep8/whitespace_naming_convention2.py
+++ lab/refactoring-pep8/whitespace_naming_convention2.py
@@ -1,32 +1,41 @@
 # By Kami Bigdely
 # PEP8 - whitespaces and variable names.
-class pizza:
-    def __init__ (obj, mybread_type,CHEESE_TYPE,meatType,pizza_toppings,size):
-        obj.bread_type= mybread_type
-        obj.cheese_type = CHEESE_TYPE
-        obj.meatType= meatType
-        obj.toppings = pizza_toppings
-        obj.size = size        
+
+
+class Pizza:
+
+    def __init__(self, my_bread_type, CHEESE_TYPE,
+                  meat_type, pizza_toppings, size):
+        self.bread_type= my_bread_type
+        self.cheese_type = CHEESE_TYPE
+        self.meatType= meat_type
+        self.toppings = pizza_toppings
+        self.size = size 
+
     @classmethod
-    def Create_ChicagoPizza (class_type, size):
+    def create_chicago_pizza(cls, size):
         bread = 'deep-dish bread'
         cheese = 'mozzarella cheese'
-        meatType= 'Italian sausage'
-        toppings = ['green bell pepper','mushroom', 'chunky tomato sauce', 'onion']
-        return class_type (bread, cheese, meatType, toppings, size)    
+        meat_type= 'Italian sausage'
+        toppings = ['green bell pepper','mushroom', 
+                    'chunky tomato sauce', 'onion']
+        return cls(bread, cheese, meat_type, toppings, size)  
+
     @classmethod
-    def createCalifornia_pizza(ct, meat_Type,size):
+    def create_california_pizza(cls, meat_type, size):
         bread = 'thin crust'
         CHEESE = 'feta cheese'
-        toppings =[ 'garlic', 'spinach', 'broccoli', 'olives', 'red onion', 'red bell pepper' ]
-        return ct(bread, CHEESE, meat_Type, toppings, size) 
-    def printInfo(obj):
-        print('bread type is: ', obj.bread_type)
-        print('cheese type is: ', obj.cheese_type)
-        print('meat type is: ', obj.meatType)
+        toppings =['garlic', 'spinach', 'broccoli', 
+                   'olives', 'red onion', 'red bell pepper']
+        return cls(bread, CHEESE, meat_type, toppings, size) 
+
+    def print_info(self):
+        print('bread type is: ', self.bread_type)
+        print('cheese type is: ', self.cheese_type)
+        print('meat type is: ', self.meatType)
         print('Toppings are: ', end='')
-        print(', '.join(map(str, obj.toppings)))
+        print(', '.join(map(str, self.toppings)))
 
     
-myPizza = pizza.createCalifornia_pizza('chicken', 'large')
-myPizza.printInfo()
+my_pizza = Pizza.create_california_pizza('chicken', 'large')
+my_pizza.printInfo()
