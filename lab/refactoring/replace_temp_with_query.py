fd8763c8ab11eb01f60ab05e1dbc114fabb29bcf
= <zainr7989@gmail.com>
2021-02-04 15:11:21 -0500

    Memoize and refactor replace_temp_with_query.

--- lab/refactoring/replace_temp_with_query.py
+++ lab/refactoring/replace_temp_with_query.py
@@ -1,42 +1,61 @@
 # By Kamran Bigdely Nov. 2020
 # Replace temp variable with query
-# TODO: Use 'extract method' refactoring technique to improve this code. If required, use
-#       'replace temp variable with query' technique to make it easier to extract methods.
+# Use 'extract method' refactoring technique to improve this code. If required, use
+# 'replace temp variable with query' technique to make it easier to extract methods.
+
+
 class Employer:    
+
     def __init__(self, name):
         self.name = name
-    def send(self, students):
+
+    def send(self):
         print("Students' contact info were sent to", self.name + '.')
 
+
 class Student:
+
     def __init__(self, gpa, name):
         self.gpa = gpa
         self.name = name
+
     def get_gpa(self):
         return self.gpa
+
     def send_congrat_email(self):
         print("Congrats", self.name + ". You graduated successfully!")
 
+
+class Memoize:
+
+    def __init__(self, func):
+        self.func = func
+        self.cache = dict()
+
+    def __call__(self, arg):
+        if arg not in self.cache:
+            self.cache[arg] = self.func(arg)
+        return self.cache[arg]
+
+
 class School:
+
     def __init__(self, students) -> None:
         self.students = students
-    def process_graduation(self):
-        # Find the students in the school who have successfully graduated.
-        min_gpa = 2.5 # minimum acceptable GPA
-        passed_students = []
-        for s in self.students:
-            if s.get_gpa() > min_gpa:
-                passed_students.append(s)
 
-        # print student's name who graduated.
+    @Memoize
+    def get_passed_students(self, min_gpa):
+        return [
+            s for s in self.students if s.get_gpa() > min_gpa
+        ]
+
+    def print_graduates(self, passed_students):
         print('*** Student who graduated *** ')
         for s in passed_students:
             print(s.name)
         print('****************************')
-        # Send congrat emails to them.
-        for s in passed_students:
-            s.send_congrat_email()
-        # Find the top 10% of them and send their contact to the top employers
+
+    def share_with_employers(self, passed_students):
         passed_students.sort(key=lambda s: s.get_gpa())
         percentile = 0.9
         index = int(percentile * len(passed_students))
@@ -45,7 +64,20 @@ class School:
         for e in top_employers:
             e.send(top_10_percent)
 
-students = [Student(2.1, 'donald'), Student(2.3, 'william'), Student(2.7, 'toro'), 
-            Student(3.9, 'lili'), Student(3.2,'kami'), Student(3,'sarah')]
-school  = School(students)
-school.process_graduation()
+    def process_graduation(self):
+        # Find the students in the school who have successfully graduated.
+        passed_students = self.get_passed_students(2.5)
+        # print student's name who graduated.
+        self.print_graduates(passed_students)
+        # Send congrat emails to them.
+        for s in passed_students:
+            s.send_congrat_email()
+        # Find the top 10% of them and send their contact to the top employers
+        self.share_with_employers(passed_students)
+
+
+if __name__ == "__main__":
+    students = [Student(2.1, 'donald'), Student(2.3, 'william'), Student(2.7, 'toro'), 
+                Student(3.9, 'lili'), Student(3.2,'kami'), Student(3,'sarah')]
+    school  = School(students)
+    school.process_graduation()
