69e1f18d307a9cca6a8550ea4375b0d9008f2d87
= <zainr7989@gmail.com>
2021-02-04 14:15:26 -0500

    Refactor introduce_explaining_variable3.

--- lab/refactoring/introduce_explaining_variable3.py
+++ lab/refactoring/introduce_explaining_variable3.py
@@ -4,9 +4,15 @@ WELL_DONE = 900000
 MEDIUM = 600000
 COOKED_CONSTANT = 0.05
 
+
+def get_cooking_state(time, temperature, pressure):
+    return time * temperature * pressure * COOKED_CONSTANT
+
+
 def is_cookeding_criteria_satisfied(time, temperature, pressure, desired_state):
-    if desired_state == 'well-done' and time * temperature * pressure * COOKED_CONSTANT >= WELL_DONE: 
+    cooked_state = get_cooking_state(time, temperature, pressure)
+    if desired_state == 'well-done' and cooked_state >= WELL_DONE: 
         return True
-    if desired_state == 'medium' and time * temperature * pressure * COOKED_CONSTANT >= MEDIUM:
+    if desired_state == 'medium' and cooked_state >= MEDIUM:
         return True
     return False
