5cceb0252b9636dddb915387175efefebf7503d7
= <zainr7989@gmail.com>
2021-02-04 13:29:37 -0500

    Refactor inline_method module.

--- lab/refactoring/inline_method.py
+++ lab/refactoring/inline_method.py
@@ -2,24 +2,30 @@
 # Inline method.
 # TODO: Refactor this program to improve its readability.
 
-LEGAL_DRINKING_AGE = 18
+
 class Person:
+
+    LEGAL_DRINKING_AGE = 18
+
     def __init__(self, my_age):
+        '''Instantiates a new Person object with an age (a float value).'''
         self.age = my_age
-        
+
+
 def enter_night_club(individual):
-    if older_than_18_year_old(individual.age):
+    """Displays if the person is allowed to enter the club.
+
+    Args:
+        individual: a Person instance.
+    
+    Returns: None
+    """
+    if individual.age > Person.LEGAL_DRINKING_AGE:
         print("Allowed to enter.")
     else:
         print("Enterance of minors is denited.")
 
-def older_than_18_year_old(age):
-    if age > LEGAL_DRINKING_AGE:
-        return True
-    else: 
-        return False
-    
-    
-person = Person(17.9)
-enter_night_club(person)
-        
+
+if __name__ == "__main__": 
+    person = Person(17.9)
+    enter_night_club(person)
