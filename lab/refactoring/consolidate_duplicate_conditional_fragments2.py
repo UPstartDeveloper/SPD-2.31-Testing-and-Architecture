64322762ecb250dc4bf9276bab4a6d24a8dce944
= <zainr7989@gmail.com>
2021-02-11 15:06:40 -0500

    refactor consolidate duplicate conditional fragments 2.

--- lab/refactoring/consolidate_duplicate_conditional_fragments2.py
+++ lab/refactoring/consolidate_duplicate_conditional_fragments2.py
@@ -12,17 +12,20 @@ def display_gear(str_gear):
 
 
 def process_speed(speed):
+    # map the speed bounds to their gears
+    bounds = [
+        (float("-inf"), 0), (0, 30),
+        (30, 50), (50, 90), (90, float("inf"))
+    ]
+    gears = ['R', '1', '2', '3', '4']
+    bounds_to_gears = dict(zip(bounds, gears))
+    # define the gear to use
     gear = ''
-    if 0 <= speed < 30:
-        gear = '1'
-    elif 30 <= speed < 50:
-        gear = '2'
-    elif 50 <= speed <= 90:
-        gear = '3'
-    elif 90 <= speed:
-        gear = '4'
-    elif speed <= 0:
-        gear = 'R'
+    for lower_limit, upper_limit in bounds:
+        if lower_limit <= speed < upper_limit:
+            gear = bounds_to_gears[(lower_limit, upper_limit)]
+            break
+    # change the gear
     change_gear(gear)
     display_gear(gear)
 
