c940c11169254453bb62720ff1deb82efb8cf9f4
= <zainr7989@gmail.com>
2021-02-11 14:22:53 -0500

    Refactor using remove control flag and replace nested conditional with guard clauses.

--- lab/refactoring/remove_control_flag3.py
+++ lab/refactoring/remove_control_flag3.py
@@ -2,14 +2,19 @@
 # Remove control flag
 # Reference: https://searchcode.com/file/92870153/frameworkconsole/framework.py/
 
+MESSAGE = (
+    "Puts the Android Agent inside an Android App APK. \
+    The application runs normally, with extra functionality."
+)
+
+
 def backdoor_srcmethod():
-    running = True
-    while running:
-        print( "Puts the Android Agent inside an Android App APK. The application runs normally, with extra functionality.")
+    print(MESSAGE)
+    inputfile = input('APK to Backdoor: ').strip()
+    while (inputfile == '') is False:
+        print('doing other stuff.') 
+        print(MESSAGE)
         inputfile = input('APK to Backdoor: ').strip()
-        if inputfile == '':
-            running = False
-        else:
-            print('doing other stuff.') 
+
 
 backdoor_srcmethod()
--- lab/refactoring/replace_nested_conditional_with_gaurd_clauses.py
+++ lab/refactoring/replace_nested_conditional_with_gaurd_clauses.py
@@ -1,19 +1,14 @@
 # by Kami Bigdely
-# Replace nested conditional with gaurd clauses
+# Replace nested conditional with gaurd clauses 
 
 def extract_position(line):
-    if not line:
-        pos = None
+    if line is not None and 'x:' in line:
+        start_index = line.find('x:') + 2
+        pos = line[start_index:] # from start_index to the end.
     else:
-        if 'debug' in line or 'error' in line:
-            pos = None
-        else:
-            if 'x:' in line:
-                start_index = line.find('x:') + 2
-                pos = line[start_index:] # from start_index to the end.
-            else: 
-                pos = None
+        pos = None
     return pos
+
 
 if __name__ == "__main__":
     result1 = extract_position('|error| numerical calculations could not converge.')
